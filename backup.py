ae394d69157442312593f744cffe2ab88436897c
Alexander Bangert <ab@xentary.de>
2014-07-08 09:40:24 +0200

    copy&paste error

--- backup.py
+++ backup.py
@@ -11,7 +11,7 @@ BUCKET = "gendis-zuelch"
 BUCKET_FOLDER = "patlocal"
 
 if os.path.exists(FOLDER):
-        shutil.rmtree(FOLDER)
+	shutil.rmtree(FOLDER)
 os.makedirs(FOLDER)
 
 db=MySQLdb.connect()
@@ -19,12 +19,12 @@ c = db.cursor()
 c.execute("""SHOW DATABASES""")
 
 for row in c.fetchall():
-        name = row[0]
-        if name in IGNORE_DATABASES:
-                continue
-        cmd = "mysqldump --events {0} > {1}/{2}.sql".format(name, FOLDER, name)
-        os.system(cmd)
+	name = row[0]
+	if name in IGNORE_DATABASES:
+		continue
+	cmd = "mysqldump --events {0} > {1}/{2}.sql".format(name, FOLDER, name)
+	os.system(cmd)
 
 os.system("tar cvfz `hostname`_`date +%Y%m%d`.tar.gz backup")
-os.system("s3cmd put `hostname`_`date +%Y%m%d`.tar.gz s3://gendis-zuelch/patlocal/`date +%Y%m%d`_`ho$
+os.system("s3cmd put `hostname`_`date +%Y%m%d`.tar.gz s3://gendis-zuelch/patlocal/`date +%Y%m%d`_`hostname`.tar.gz")
 os.system(""" /usr/local/lib/backup/expire.sh "gendis-zuelch/patlocal" "30 Days" """)
